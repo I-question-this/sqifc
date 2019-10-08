1cf02a607df695d0a8d60d18fcd9d5836492681a
Tyler Westland <tylerofthewest@gmail.com>
2019-10-08 14:42:21 -0400

    Adds comments.

--- test/driver.c
+++ test/driver.c
@@ -16,11 +16,14 @@ int main(void)
 	int K = 32;
 	
 	int output = func();
-	
+
+	// Create a bit vector of the output	
 	int bv[K];
 	for(int i = 0; i < K; i++)
 		bv[i] = (output >> i) & 1;
 
+	// Assert the specific bit were are interested in.
+	// SQIFC changes the assertion for each bit.
 	assert (bv[0] == 0);
 	return 0;
 }
